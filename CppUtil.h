e44a47a9f72d926db0eadc5031d1658d14db836a
Dan <doates625@gmail.com>
2019-10-18 10:50:53 -0700

    Cleaned up header file and added README

--- CppUtil.h
+++ CppUtil.h
@@ -1,11 +1,13 @@
 /**
  * @file CppUtil.h
- * @brief Collection of common useful C++ functions.
+ * @brief Collection of common useful C++ functions
  * @author Dan Oates (WPI Class of 2020)
  */
 #include <stdint.h>
 
-// Floating-Point Operations
+/**
+ * Floating-Point Operations
+ */
 float clamp_limit(float val, float val_min, float val_max);
 float mod_limit(float val, float val_min, float val_max);
 float fadd(float a, float b);
@@ -13,10 +15,16 @@ float fsub(float a, float b);
 float fmul(float a, float b);
 float fdiv(float a, float b);
 float fsq(float n);
+
+/**
+ * Floating-Point Array Operations
+ */
 float mean(float arr[], uint32_t len);
 float variance(float arr[], float mean, uint32_t len);
 float stdev(float arr[], float mean, uint32_t len);
 
-// Integer Operations
+/**
+ * Integer Operations
+ */
 int min(int a, int b);
 int max(int a, int b);
